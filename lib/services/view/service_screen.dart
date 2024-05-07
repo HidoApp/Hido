fc0a43a4e76d2dfaf00f66be9947864abac1603c
Ron484 <rhobah48@hotmail.com>
2024-05-07 09:52:11 +0300

    fix service

--- lib/services/view/service_screen.dart
+++ lib/services/view/service_screen.dart
@@ -17,11 +17,8 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:ajwad_v4/explore/tourist/view/notification/notification_screen.dart';
-
 import 'package:get/get.dart';
 import 'tabs/hospitality_tab.dart';
-
 
 class ServiceScreen extends StatefulWidget {
   const ServiceScreen({super.key});
@@ -31,9 +28,7 @@ class ServiceScreen extends StatefulWidget {
 }
 
 class _ServiceScreenState extends State<ServiceScreen>
-
     with SingleTickerProviderStateMixin {
-
   late TabController _tabController;
   int _tabIndex = 0;
 
@@ -116,186 +111,6 @@ class _ServiceScreenState extends State<ServiceScreen>
                   width: width,
                   fit: BoxFit.cover,
                 ),
-                Positioned(
-                  top: height * 0.035,
-                  left: 16,
-                  right: 16,
-                  child: Column(
-                  crossAxisAlignment: AppUtil.rtlDirection2(context) ? CrossAxisAlignment.start : CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 126,
-                        height: 34,
-                        decoration: BoxDecoration(
-                            color: Colors.black.withOpacity(0.2),
-                            borderRadius: BorderRadius.circular(16)),
-                        child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        textDirection: TextDirection.ltr,
-
-
-                          children: [
-                            //             InkWell(
-                            //               onTap: () {},
-                            //               child: Container(
-                            //                 height: 36,
-                            //                 width: width * 0.6,
-                            //                 decoration: BoxDecoration(
-                            //                //   color: Colors.white,
-                            //                   borderRadius: BorderRadius.circular(18),
-                            //                 ),
-                            //                 alignment: Alignment.center,
-                            //                 // child: Row(
-                            //                 //   children: [
-                            //                 //     const SizedBox(
-                            //                 //       width: 22,
-                            //                 //     ),
-                            //                 //     SvgPicture.asset(
-                            //                 //       'assets/icons/search.svg',
-                            //                 //     ),
-                            //                 //     const SizedBox(
-                            //                 //       width: 12,
-                            //                 //     ),
-                            //                 //     CustomText(
-                            //                 //       text: 'search'.tr,
-                            //                 //       fontSize: 14,
-                            //                 //       fontWeight: FontWeight.w400,
-                            //                 //       color: thinGrey.withOpacity(0.5),
-                            //                 //     ),
-                            //                 //   ],
-                            //                 // ),
-                            //               ),
-                            //             ),
-                            //              InkWell(
-                            //            onTap: () {
-                            //               ProfileController _profileController = Get.put(ProfileController());
-                            //             Get.to(() =>  AppUtil.isGuest()
-                            // ? const SignInScreen()
-                            // : MessagesScreen(  profileController: _profileController));
-                            //           },
-                            //           child: Container(
-                            //             width: 36,
-                            //             height: 36,
-                            //             decoration: const BoxDecoration(
-                            //               shape: BoxShape.circle,
-                            //               color: pink,
-                            //             ),
-                            //             alignment: Alignment.center,
-                            //             child: SvgPicture.asset('assets/icons/message.svg'),
-                            //           ),
-                            //         ),
-
-                            // InkWell(
-                            //   onTap: () {
-                            //     ProfileController _profileController =
-                            //         Get.put(ProfileController());
-                            //     Get.to(() => AppUtil.isGuest()
-                            //         ? const SignInScreen()
-                            //         : MessagesScreen(
-                            //             profileController: _profileController));
-                            //   },
-                            InkWell(
-  onTap: () {
-    Get.to(() => AppUtil.isGuest()
-        ? const SignInScreen()
-        : TouristChatScreen(isChat: true));
-  },
-                              child: Container(
-                                width: 36,
-                                height: 36,
-                                alignment: Alignment.center,
-                                child: SvgPicture.asset(
-                                    'assets/icons/Communication_white.svg'),
-                              ),
-                            ),
-
-                            InkWell(
-                              onTap: () {
-                                ProfileController _profileController =
-                                    Get.put(ProfileController());
-                                Get.to(() => AppUtil.isGuest()
-                                    ? const SignInScreen()
-                                    : TicketScreen(
-                                        profileController: _profileController));
-                              },
-                              child: Container(
-                                width: 36,
-                                height: 36,
-                                alignment: Alignment.center,
-                                child: SvgPicture.asset(
-                                    'assets/icons/ticket_icon.svg'),
-                              ),
-                            ),
-
-                            InkWell(
-                              onTap: () {
-                                Get.to(() => AppUtil.isGuest()
-                                    ? const SignInScreen()
-                                    // : Scaffold(
-                                    //     body: Center(
-                                    //     child: CustomText(text: 'Comming soon'),
-                                    //   ))
-                                     :NotificationScreen(hasNotifications: true) // Change false to whatever value you need
-
-                                      
-                                      );
-                              },
-                              child: Container(
-                                width: 36,
-                                height: 36,
-                                alignment: Alignment.center,
-                                child: SvgPicture.asset(
-                                    'assets/icons/Alerts_white.svg'),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: height * 0.1,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                         // right: AppUtil.rtlDirection(context) ? 8 : 0,
-                          //left: AppUtil.rtlDirection(context) ? 0 : 8,
-                        right: (_tabIndex == 3) ? (AppUtil.rtlDirection2(context) ? 0 : 155) : (_tabIndex == 1)? (AppUtil.rtlDirection2(context) ? 0 : 49):(AppUtil.rtlDirection2(context) ? 0 : 65),
-                        left: (_tabIndex == 3) ? (AppUtil.rtlDirection2(context) ? 225 : 0) : (_tabIndex == 1)?(AppUtil.rtlDirection2(context) ? 133 : 0) :(AppUtil.rtlDirection2(context) ? 89 : 0),
-
-                        ),
-                        child: CustomText(
-                          text: (_tabIndex == 0
-                                  ? ''
-                                  : _tabIndex == 2
-                                      ? 'serviceAdventureTitle'
-                                      : _tabIndex == 1
-                                          ? 'serviceEventTitle'
-                                          : 'serviceFoodTitle')
-                              .tr,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w700,
-                          fontSize: 32,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                          //right: AppUtil.rtlDirection(context) ? 8 : 0,
-                          //left: AppUtil.rtlDirection(context) ? 0 : 8,
-                          right: (_tabIndex == 2) ? (AppUtil.rtlDirection2(context) ? 0 : 35) :AppUtil.rtlDirection2(context) ? 0 : 4,
-                          left: (_tabIndex == 1) ? (AppUtil.rtlDirection2(context) ? 95 : 0) : AppUtil.rtlDirection2(context) ? 85 : 0,
-                        ),
-                        child: CustomText(
-                          text: (_tabIndex == 0
-                                  ? ''
-                                  : _tabIndex == 2
-                                      ? 'serviceAdventureSubtitle'
-                                      : _tabIndex == 1
-                                          ? 'serviceEventSubtitle'
-                                          : 'serviceFoodSubtitle')
-                              .tr,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
               ),
               bottom: TabBar(
                 controller: _tabController,
