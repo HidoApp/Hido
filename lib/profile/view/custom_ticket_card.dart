b7eb4db55f91b3d7b86b671302225b0185ebf865
Ron484 <rhobah48@hotmail.com>
2024-05-15 01:11:45 +0300

    fix ticket

--- lib/profile/view/custom_ticket_card.dart
+++ lib/profile/view/custom_ticket_card.dart
@@ -11,87 +11,93 @@ import 'package:ajwad_v4/explore/tourist/model/place.dart';
 import 'package:ajwad_v4/request/tourist/view/find_ajwady.dart';
 import 'package:intl/intl.dart';
 
+
+
 class CustomTicketCard extends StatelessWidget {
   const CustomTicketCard({
     super.key,
     required this.booking,
+
+    
   });
 
   final Booking booking;
 
   @override
   Widget build(BuildContext context) {
-    final TouristExploreController _touristExploreController =
-        Get.put(TouristExploreController());
-    Place? thePlace;
+     final TouristExploreController _touristExploreController =
+      Get.put(TouristExploreController());
+  Place? thePlace;
 
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
     return InkWell(
+   onTap: booking.orderStatus == 'ACCEPTED'
+      ? () {
+          Get.to(() => TicketDetailsScreen(booking: booking));
+        }
+      : booking.orderStatus == 'PENDING' ? () {
+          // _touristExploreController
+          //     .getPlaceById(
+          //       id:booking!.placeId!,
+          //       context: context,
+          //     )
+          //     .then((place) {
+          //   thePlace = place;
+          //   Get.to(
+          //     () => FindAjwady(
+          //       booking: thePlace!.booking![0],
+          //       place: booking.place!,
+          //       placeId: thePlace!.id!,
+          //     ),);
+          //   //   ?.then((value) {
+          //   //   return getPlaceBooking();
+          //   // });
+          // });
+         Get.to(() => TicketDetailsScreen(booking: booking));
 
-      onTap: booking.orderStatus == 'ACCEPTED'
-          ? () {
-              Get.to(() => TicketDetailsScreen(booking: booking));
-            }
-          : () {
-              _touristExploreController
-                  .getPlaceById(
-                id: booking!.placeId!,
-                context: context,
-              )
-                  .then((place) {
-                thePlace = place;
-                Get.to(
-                  () => FindAjwady(
-                    booking: thePlace!.booking![0],
-                    place: booking.place!,
-                    placeId: thePlace!.id!,
-                  ),
-                );
-                //   ?.then((value) {
-                //   return getPlaceBooking();
-                // });
-              });
-            },
-      child: Container(
-        width: 334,
-        height: 110,
-        child: Card(
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(7.36))),
-          color: Colors.white,
-          child: Padding(
-            padding:
-                const EdgeInsets.only(bottom: 10, left: 9, right: 8, top: 8),
-            //.copyWith(
-            //     right: !AppUtil.rtlDirection(context) ? 10 : 30,
-            //     left: !AppUtil.rtlDirection(context) ? 30 : 10,
+        }
+        :() {
+        },
 
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              //  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  child: Container(
+  width: 334,
+  height: 120,
+  child: Card(
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(7.36))),
+    color: const Color.fromARGB(255, 255, 255, 255),
+    child: Padding(
+      padding: const EdgeInsets.only(bottom: 10, left: 9, right: 8, top: 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              child: Image.network(
+                booking.place!.image![0],
+                height: height * 0.08,
+                width: width * 0.2,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 8,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(bottom: 12),
-                  child: ClipRRect(
-                      borderRadius: const BorderRadius.all(Radius.circular(8)),
-                      child: Image.network(
-                        booking.place!.image![0],
-                        height: height * 0.08,
-                        width: width * 0.2,
-                        fit: BoxFit.cover,
-                      )),
-                ),
-                SizedBox(
-                  width: 12,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 6),
-                      child: CustomText(
+                  padding: const EdgeInsets.only(top: 6),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomText(
                         text: AppUtil.rtlDirection(context)
                             ? booking.place!.nameEn!
                             : booking.place!.nameAr!,
@@ -100,6 +106,31 @@ class CustomTicketCard extends StatelessWidget {
                         fontFamily: 'SF Pro',
                         color: black,
                       ),
+                      Row(
+                        children: [
+                          SvgPicture.asset('assets/icons/Polygon_host.svg'),
+                          const SizedBox(
+                            width: 4,
+                          ),
+                          CustomText(
+                            text: 'Tour',
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500,
+                            color: black,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  children: [
+                    SvgPicture.asset('assets/icons/map_pin.svg'),
+                    const SizedBox(
+                      width: 4,
                     ),
                     CustomText(
                       text: AppUtil.rtlDirection2(context)
@@ -120,156 +151,48 @@ class CustomTicketCard extends StatelessWidget {
                       'assets/icons/grey_calender.svg',
                     ),
                     const SizedBox(
-                      height: 5,
+                      width: 4,
                     ),
-
-                    Row(
-                      children: [
-                        SvgPicture.asset('assets/icons/map_pin.svg'),
-                        const SizedBox(
-                          width: 4,
-                        ),
-                        CustomText(
-                          text: AppUtil.rtlDirection2(context)
-                              ? booking.place!.regionAr!
-                              : booking.place!.regionEn!,
-                          fontSize: 11,
-                          fontWeight: FontWeight.w400,
-                          color: textGreyColor,
-                        ),
-                      ],
+                    CustomText(
+                      text: DateFormat.yMMMMd().format(DateTime.parse(booking.date)),
+                      fontSize: 10,
+                      fontWeight: FontWeight.w400,
+                      color: textGreyColor,
+                    ),
+                     ],
+                ),
+                    
+                   SizedBox(
+                       height: 5,
+                   ),
+                   Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.end,
+                    children:[
+                    SvgPicture.asset(
+                      'assets/icons/${booking.orderStatus! == 'ACCEPTED' ? 'confirmed.svg' : booking.orderStatus! == 'CANCELED'? 'canceled.svg' : 'waiting.svg'}',
                     ),
                     const SizedBox(
-                      height: 5,
+                      width: 4,
                     ),
-
-                    Row(
-                      children: [
-                        SvgPicture.asset(
-                          'assets/icons/grey_calender.svg',
-                        ),
-                        const SizedBox(
-                          width: 4,
-                        ),
-                        CustomText(
-                          text: DateFormat.yMMMMd()
-                              .format(DateTime.parse(booking.date)),
-                          fontSize: 10,
-                          fontWeight: FontWeight.w400,
-                          color: textGreyColor,
-                        ),
-                        SizedBox(
-                          width: width * 0.19,
-                        ),
-                        SvgPicture.asset(
-                            'assets/icons/${booking.orderStatus! == 'ACCEPTED' ? 'confirmed.svg' : 'waiting.svg'}'),
-                        const SizedBox(
-                          width: 4,
-                        ),
-                        CustomText(
-                          text: booking.orderStatus!,
-                          fontSize: 10,
-                          fontWeight: FontWeight.w400,
-                          color: colorGreen,
-                        ),
-                        // const SizedBox(
-                        //   width: 5,
-                        // ),
-                        // Container(
-                        //   width: 5,
-                        //   height: 5,
-                        //   decoration: const BoxDecoration(
-                        //     color: colorGreen,
-                        //     shape: BoxShape.circle,
-                        //   ),
-                        // ),
-                        // const SizedBox(
-                        //   width: 5,
-                        // ),
-                        // CustomText(
-                        //   text: '${booking.timeToGo} ',
-                        //   fontSize: 10,
-                        //   fontWeight: FontWeight.w400,
-                        //   color: colorGreen,
-                        // ),
-                      ],
+                    
+                    CustomText(
+                      text: booking.orderStatus!,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w400,
+                      color: booking.orderStatus! == 'ACCEPTED' ? colorGreen : booking.orderStatus! == 'CANCELED'? Color(0xFFDC362E) : colorDarkGrey,
                     ),
-                    // const SizedBox(
-                    //   height: 8,
-                    // ),
-                    // CustomText(
-                    //   text: AppUtil.rtlDirection(context)
-                    //       ? booking.place!.nameEn!
-                    //       : booking.place!.nameAr!,
-                    //   fontSize: 18,
-                    //   fontWeight: FontWeight.w500,
-                    //   color: black,
-                    // ),
-                    // const SizedBox(
-                    //   height: 8,
-                    // ),
-                    // Row(
-                    //   children: [
-                    //     SvgPicture.asset('assets/icons/map_pin.svg'),
-                    //     const SizedBox(
-                    //       width: 4,
-                    //     ),
-                    //     CustomText(
-                    //       text: AppUtil.rtlDirection(context)
-                    //           ? booking.place!.regionEn!
-                    //           : booking.place!.regionAr!,
-                    //       fontSize: 10,
-                    //       fontWeight: FontWeight.w400,
-                    //       color: textGreyColor,
-                    //     ),
-                    //     SizedBox(
-                    //       width: width * 0.2,
-                    //     ),
-                    //     SvgPicture.asset(
-                    //         'assets/icons/${booking.orderStatus! == 'ACCEPTED' ? 'confirmed.svg' : 'waiting.svg'}'),
-                    //     const SizedBox(
-                    //       width: 4,
-                    //     ),
-                    //     CustomText(
-                    //       text: booking.orderStatus!,
-                    //       fontSize: 10,
-                    //       fontWeight: FontWeight.w400,
-                    //       color: colorGreen,
-                    //     ),
-                    //   ],
-                    // ),
-                  ],
-                ),
-                //   Row(
-                //     children: [
-
-                //       CustomText(
-                //         text:booking.orderStatus!,
-                //         //  status == 'canceled'
-                //         //     ? 'canceled'.tr
-                //         //     : status == 'waiting'
-                //         //         ? 'waiting'.tr
-                //         //         :
-                //                // 'confirmed'.tr,
-                //         fontSize: 10,
-                //         fontWeight: FontWeight.w400,
-                //         color:
-
-                //         // status == 'canceled'
-                //         //     ? colorRed
-                //         //     : status == 'waiting'
-                //         //         ? yellow
-                //         //         :
-
-                //                 colorGreen,
-                //       ),
-                //     ],
-                //   ),
+                ],
+                 ),
               ],
             ),
           ),
-        ),
+        ],
       ),
+    ),
+  ),
+),
+
     );
   }
 }
