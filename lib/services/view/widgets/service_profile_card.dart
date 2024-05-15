f25649b6343fc2b353b0f5b5814c9ec2cd741826
AmmarHido <ammar@hido.app>
2024-05-15 16:45:50 +0300

    nothing yet

--- lib/services/view/widgets/service_profile_card.dart
+++ lib/services/view/widgets/service_profile_card.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 
 class ServicesProfileCard extends StatelessWidget {
   const ServicesProfileCard(
-      {super.key, required this.name, required this.image, required this.onTap});
+      {super.key,
+      required this.name,
+      required this.image,
+      required this.onTap});
   final String name;
   final String image;
   final void Function() onTap;
